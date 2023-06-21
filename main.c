8792fb91deb1dbed04cb6461e91ad2f4b44ea138
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-21 11:43:43 +0000

    Added handle_input.c monty.h

--- main.c
+++ main.c
@@ -2,23 +2,43 @@
 
 /**
  * main - main function for the monty program
- * @ac: total number of arguments
- * @av: the arguments
+ * @argc: total number of arguments
+ * @argv: the arguments
  * Return: 0 on success
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	stack_t *stack;
 
 	stack = NULL;
-	if (ac != 2)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/*handle input*/
+	get_input(stack, argv[1]);
 
-	/*free stack here*/
+
+	free_dlistint(stack);
 	return (0);
+}
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: head of the list
+ * Return: nothing
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *holder, *temp;
+
+	holder = head;
+	while (holder != NULL)
+	{
+		temp = holder->next;
+		free(holder);
+		holder = temp;
+	}
 }
