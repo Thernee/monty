3303fd43a7b48c4308f15b1841afa840bc3df396
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-22 14:27:19 +0000

    x

--- beta/globals.c
+++ beta/globals.c
@@ -1,7 +1,9 @@
 #include "monty.h"
 
 global_vars *glob_vars = NULL;
-
+/**
+ * initializeGlobals - Allocates memory and initializes global vars
+ */
 void initializeGlobals(void)
 {
 	glob_vars = malloc(sizeof(global_vars));
@@ -10,6 +12,9 @@ void initializeGlobals(void)
 	glob_vars->op_args = 0;
 }
 
+/**
+ * cleanupGlobals - cleans up global vars
+ */
 void cleanupGlobals(void)
 {
 	if (glob_vars != NULL)
